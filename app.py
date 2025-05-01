8aa839e0205fc106fd6c34664767202fd21357b7
yvendee <71810479+yvendee@users.noreply.github.com>
2025-05-01 15:00:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify, request, render_template
 # from swagger.swaggerui import setup_swagger
 # import random
 # import string
-
+import requests
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/static')
 
@@ -17,6 +17,30 @@ app = Flask(__name__, template_folder='templates', static_folder='static', stati
 # def index():
 #     return jsonify({"message": "Welcome to the appfinity API"})
 
+@app.route('/read', methods=['GET'])
+def read():
+    name = request.args.get('name')
+    if not name:
+        return jsonify({"error": "Missing 'name' parameter"}), 400
+
+    data = {
+        "name": name,
+        "section": "A",
+        "status": "IN"
+    }
+
+    response = requests.post(
+        "http://152.42.247.62:9000/log_karen_attendance",
+        json=data,
+        headers={"Content-Type": "application/json"}
+    )
+
+    return jsonify({
+        "sent_data": data,
+        "external_response": response.text,
+        "status_code": response.status_code
+    })
+    
 @app.route('/', methods=['GET'])
 def index():
     # Render the HTML template
